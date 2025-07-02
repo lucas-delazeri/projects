6686b74ac799d0a0a271fd8d42711c1bf0d52210
Lucas Souza <lucasdelazeris@gmail.com>
2025-07-02 11:10:24 -0300

    Update Main.java

--- BankAccount/Main.java
+++ BankAccount/Main.java
@@ -6,30 +6,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Informe os dados da conta");
-        System.out.println("Numero:");
+        System.out.println("Enter account details");
+        System.out.println("Number: ");
         int number = sc.nextInt();
 
         sc.nextLine();
 
-        System.out.println("Titular");
+        System.out.println("Holder");
         String holder = sc.nextLine();
 
-        System.out.println("Saldo inicial");
+        System.out.println("Opening balance");
         double balance = sc.nextDouble();
 
-        System.out.println("Limite");
+        System.out.println("Limit");
         double WithDrawLimit = sc.nextDouble();
 
         Account acc = new Account(number, holder, balance, WithDrawLimit);
 
         System.out.println();
-        System.out.print("Informe a quantia a ser sacada: ");
+        System.out.print("Enter the amount to be withdrawn:");
         double amount = sc.nextDouble();
 
         try {
             acc.withDraw(amount);
-            System.out.println("Novo saldo: " + Account.getBalance());
+            System.out.println("New balance: " + Account.getBalance());
         } catch (BusinessException e) {
             System.out.println(e.getMessage());
         }
